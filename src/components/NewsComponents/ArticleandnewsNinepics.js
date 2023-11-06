4a6810f6d44b815502f5f323692b61ceec130a87
Jane <hezha.aban@gmail.com>
2023-11-06 16:03:17 +0100

    klar med bilderna ska bara fixa bild 3

--- src/components/NewsComponents/ArticleandnewsNinepics.js
+++ src/components/NewsComponents/ArticleandnewsNinepics.js
@@ -8,7 +8,7 @@ const ArticleandnewsNinepics = ({imageSrc, title, customClass, category}) => {
   return (
     <>
     <div className="text-under-img">
-        <a href=""><img src={imageSrc} alt="a picture of a woman smiling"/></a>
+        <a className="img-api" href=""><img src={imageSrc} alt=""/></a>
         <p id="business">{category}</p>
         <h4>{title}</h4>
       </div>
