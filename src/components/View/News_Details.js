d04253326b6bdf27918467ff3a289eb9944ba779
Jane <hezha.aban@gmail.com>
2023-11-13 16:57:36 +0100

    fixat scss till artiklarna

--- src/components/View/News_Details.js
+++ src/components/View/News_Details.js
@@ -30,49 +30,31 @@ const News_Details = () => {
     }, [id]);
   
     if (article) {
-      const imgStyle = {
-        width: '40%',
-        height: 'auto',
-      };
-
-      const paragraphStyle = {
-        color: '#191919', 
-        fontFamily: 'Inter, sans-serif', 
-        fontSize: '18px', 
-        fontWeight: 500,
-        margin: '20px',
-      };
-
-      const titleStyle = {
-        fontFamily: 'DM Sans, sans-serif',
-          fontWeight: 700, 
-          fontSize: '30px',
-          color: '#191919', 
-          margin: '20px',
-      };
+      
     
 
     return (
-      <div >
+      <div className="news-details-div">
        <HeaderSection />
        <NewsMiniNav navPieceOne="Home" navPieceTwo="News" title="News & Articles"/>
-        <h2 style={titleStyle}>{article.title}</h2>
-        <img src={article.imageUrl} alt={article.title} style={imgStyle} />
-        <p style={paragraphStyle}>{article.content} </p>
-        <p style={paragraphStyle}>{article.author} </p>
-        <p style={paragraphStyle}>{article.published} </p>
-        <section className="black-navigation">
-    <div className="container">
-      <div className="crito">
-        <img src={blackCritoLogo} alt=""/>
-        <p>Lorem ipsum dolor sit amet consectetur<br/> adipisicing elit. Placeat obcaecati voluptas<br/> voluptates! Voluptates laborum nam<br/> ratione minus necessitatibus, iure<br/> praesentium.</p>
-    </div>
-    <BlacknavigationNews divName="small-titles" titleClass="company" title="Company" title1="About" title2="Features" title3="Works" title4="Career" />
-      <BlacknavigationNews divName="small-titles help" titleClass="help" title="Help" title1="Customer Support" title2="Delivery Details" title3="Terms & Conditions" title4="Privacy Policy" />
-      <BlacknavigationNews divName="small-titles" title="Resources" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist" />
-      <BlacknavigationNews divName="small-titles link" title="Link" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist"/>
-    </div>
-    </section>
+        <h3> {article.title}</h3>
+        <p>{article.author}</p>
+        <p>{article.published}</p>
+        <img src={article.imageUrl} alt={article.title} />
+        <p> {article.content}</p>
+        
+      <section className="black-navigation">
+        <div className="container">
+          <div className="crito">
+            <img src={blackCritoLogo} alt=""/>
+              <p>Lorem ipsum dolor sit amet consectetur<br/> adipisicing elit. Placeat obcaecati voluptas<br/> voluptates! Voluptates laborum nam<br/> ratione minus necessitatibus, iure<br/> praesentium.</p>
+          </div>
+          <BlacknavigationNews divName="small-titles" titleClass="company" title="Company" title1="About" title2="Features" title3="Works" title4="Career" />
+            <BlacknavigationNews divName="small-titles help" titleClass="help" title="Help" title1="Customer Support" title2="Delivery Details" title3="Terms & Conditions" title4="Privacy Policy" />
+            <BlacknavigationNews divName="small-titles" title="Resources" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist" />
+            <BlacknavigationNews divName="small-titles link" title="Link" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist"/>
+          </div>
+          </section>
       
         <FooterSection />
      
