d0760ed40cf7cb52391115acb436b0c1cac100e1
Jane <hezha.aban@gmail.com>
2023-11-13 19:50:13 +0100

    services klar

--- src/components/View/Services.js
+++ src/components/View/Services.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import HeaderSection from '../HeaderSection';
-import FooterSection from '../FooterSection';
-import BlacknavigationSection from '../BlacknavigationSection';
-import ServicesBody from '../ServiceComponents.js/ServicesBody';
-import NewsMiniNav from '../NewsComponents/NewsMiniNav';
-import ArticleandnewsSection from '../ArticleandnewsSection';
+import notFound from '../../assets/images/notFound2.jpg'
 
 
 
@@ -14,12 +9,7 @@ const Services = () => {
   
   return (
     <div>
-      <HeaderSection />
-      <NewsMiniNav navPieceOne="Home" navPieceTwo="News" title="News & Articles" />
-      <ServicesBody />
-      <ArticleandnewsSection backgroundColor="#F0EFE9" />
-      <BlacknavigationSection />
-      <FooterSection/>
+      <img src={notFound} />
     </div>
   )
 }
